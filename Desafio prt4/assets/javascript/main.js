499c35229184345ca698b118bad99bf64c208868
Paolo Landeros <landerospaolo@gmail.com>
2022-12-13 12:38:11 -0300

    modified desafio prt4

--- Desafio prt4/assets/javascript/main.js
+++ Desafio prt4/assets/javascript/main.js
@@ -1,41 +1,33 @@
 let changeColor;
-let firstColor = '#9acd32';
-let secondColor = '#6495ed';
-let thirdColor = '#ff0';
-let fourthColor = '#dc143c';
 
 document.addEventListener('keydown', function (event){
     if(event.key === 'a') {
-        changeColor = document.querySelector('#first');
-        changeColor.style.backgroundColor = firstColor;
+        changeColor = 'red';
     } else if(event.key === 's') {
-        changeColor = document.querySelector('#second');
-        changeColor.style.backgroundColor = secondColor;
+        changeColor = 'black';
     } else if(event.key === 'd') {
-        changeColor = document.querySelector('#third');
-        changeColor.style.backgroundColor = thirdColor;
+        changeColor = 'yellow';
     } else if(event.key === 'f') {
-        changeColor = document.querySelector('#fourth');
-        changeColor.style.backgroundColor = fourthColor;
+        changeColor = 'green';
     }
 })
 
 let color1 = document.getElementById('first').addEventListener('click', () => {
     color1 = document.getElementById('first');
-    color1.style.backgroundColor = '#f00';
+    color1.style.backgroundColor = changeColor;
 })
 
 let color2 = document.getElementById('second').addEventListener('click', () => {
     color2 = document.getElementById('second');
-    color2.style.backgroundColor = '#556b2f';
+    color2.style.backgroundColor = changeColor;
 })
 
 let color3 = document.getElementById('third').addEventListener('click', () => {
     color3 = document.getElementById('third');
-    color3.style.backgroundColor = '#ff00ff';
+    color3.style.backgroundColor = changeColor;
 })
 
 let color4 = document.getElementById('fourth').addEventListener('click', () => {
     color4 = document.getElementById('fourth');
-    color4.style.backgroundColor = '#000';
+    color4.style.backgroundColor = changeColor;
 })
